dd51e888826e49a130a93837df7260809a6559d1
llsrrll96 <llsrrll96@gmail.com>
2023-02-28 14:44:03 +0900

    init

---
+++ src/main/java/numble/server/timedeal/controller/MemberController.java
@@ -0,0 +1,17 @@
+package numble.server.timedeal.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/users")
+    public ResponseEntity<String> findByUsers(){
+        return new ResponseEntity<>("OK", HttpStatus.OK );
+    }
+}
