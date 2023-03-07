1ba4afa484a29c4ca729eae481104c6a171a03e7
llsrrll96 <llsrrll96@gmail.com>
2023-03-07 22:52:02 +0900

    feat: add method

--- src/main/java/numble/server/timedeal/model/UserEnum.java
+++ src/main/java/numble/server/timedeal/model/UserEnum.java
@@ -1,5 +1,12 @@
 package numble.server.timedeal.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum UserEnum {
-    ROLE_USER,ROLE_ADMIN
+    ROLE_USER,ROLE_ADMIN;
+
+    @JsonCreator
+    public static UserEnum from(String s){
+        return UserEnum.valueOf(s.toUpperCase());
+    }
 }
