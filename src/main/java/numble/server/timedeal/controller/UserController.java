5e1bbcff222b4d34e9d4e091bec9e031bb1de816
llsrrll96 <llsrrll96@gmail.com>
2023-03-07 22:51:08 +0900

    feat: add updateRole

--- src/main/java/numble/server/timedeal/controller/UserController.java
+++ src/main/java/numble/server/timedeal/controller/UserController.java
@@ -2,11 +2,14 @@ package numble.server.timedeal.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import numble.server.timedeal.domain.user.UserEntity;
 import numble.server.timedeal.dto.APIMessage;
+import numble.server.timedeal.dto.request.ReqRole;
 import numble.server.timedeal.dto.request.ReqSignin;
 import numble.server.timedeal.dto.request.ReqSignup;
 import numble.server.timedeal.dto.response.RespUser;
 import numble.server.timedeal.service.UserService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -47,13 +50,21 @@ public class UserController {
         return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),"회원탈퇴",true), HttpStatus.OK );
     }
 
-    @GetMapping ("/v1/users/{userid}")
+    @GetMapping("/v1/users/{userid}")
     public ResponseEntity<APIMessage<RespUser>> userInfo(@PathVariable String userid){
         return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),"회원정보",userService.findById(userid)), HttpStatus.OK );
     }
 
-    @GetMapping ("/v1/users")
-    public ResponseEntity<?> userPagination(@PageableDefault(size = 50)Pageable pageable){
+    @GetMapping("/v1/users")
+    public ResponseEntity<Page<UserEntity>> userPagination(@PageableDefault(size = 50)Pageable pageable){
         return new ResponseEntity<>(userService.userPagination(pageable), HttpStatus.OK );
+    }
+
+    @PutMapping("/v1/users/role")
+    public ResponseEntity<APIMessage<Boolean>> updateRole(@RequestBody ReqRole reqRole){
+        if(userService.updateRole(reqRole)){
+            return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(), "권한등록",true),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(), "권한등록",false),HttpStatus.OK);
     }
 }
