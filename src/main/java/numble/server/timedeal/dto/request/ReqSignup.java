99b37223c897977363b6bb0a396ece0fd7ad0744
llsrrll96 <llsrrll96@gmail.com>
2023-03-09 14:16:40 +0900

    chore: add lombok annotation

--- src/main/java/numble/server/timedeal/dto/request/ReqSignup.java
+++ src/main/java/numble/server/timedeal/dto/request/ReqSignup.java
@@ -1,8 +1,14 @@
 package numble.server.timedeal.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReqSignup {
     private String nickname;
     private String name;
