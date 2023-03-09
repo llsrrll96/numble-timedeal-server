a6a0451061b0025dd31099315e0e5bedc124dfe1
llsrrll96 <llsrrll96@gmail.com>
2023-03-09 14:18:12 +0900

    chore: modify FetchType

--- src/main/java/numble/server/timedeal/domain/timedeal/Timedeal.java
+++ src/main/java/numble/server/timedeal/domain/timedeal/Timedeal.java
@@ -18,15 +18,21 @@ public class Timedeal {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long timedealId;
     @JoinColumn(name = "productId")
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     private ProductEntity product;
     private int limitedAmount;
+    private int sale_price;
     private LocalDateTime startDatetime;
 
     @Builder
-    public Timedeal(ProductEntity product, int limitedAmount, LocalDateTime startDatetime) {
+    public Timedeal(ProductEntity product, int limitedAmount, int sale_price, LocalDateTime startDatetime) {
         this.product = product;
         this.limitedAmount = limitedAmount;
+        this.sale_price = sale_price;
         this.startDatetime = startDatetime;
+    }
+
+    public void setLimitedAmount(int limitedAmount) {
+        this.limitedAmount = limitedAmount;
     }
 }
