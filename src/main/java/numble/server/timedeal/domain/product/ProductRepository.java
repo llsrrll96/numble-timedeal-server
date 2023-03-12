81e4863a2486f8af62fc327e3dd2f592b066d6f3
llsrrll96 <llsrrll96@gmail.com>
2023-03-12 22:08:34 +0900

    chore: remove 주석

--- src/main/java/numble/server/timedeal/domain/product/ProductRepository.java
+++ src/main/java/numble/server/timedeal/domain/product/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
+
     int deleteByProductId(Long productId);
     Optional<ProductEntity> findByProductId(Long productid);
 }
