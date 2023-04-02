d0e967383832746471df37c586275f39060cbbd7
llsrrll96 <llsrrll96@gmail.com>
2023-04-03 00:15:26 +0900

    refactor: Optainal 타입으로 변경

--- src/main/java/numble/server/timedeal/domain/user/repository/UserRepository.java
+++ src/main/java/numble/server/timedeal/domain/user/repository/UserRepository.java
@@ -2,8 +2,10 @@ package numble.server.timedeal.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<UserEntity,String> {
-    UserEntity findByNickname(String nickname);
-    UserEntity findByUserId(String userId);
+    Optional<UserEntity> findByNickname(String nickname);
+    Optional<UserEntity> findByUserId(String userId);
 }
