7889d5928652088079851952c6c7bb8dcf3ac28e
llsrrll96 <llsrrll96@gmail.com>
2023-04-02 22:11:35 +0900

    refactor: 변수명 변경

--- src/main/java/numble/server/timedeal/domain/product/dto/ReqProduct.java
+++ src/main/java/numble/server/timedeal/domain/product/dto/ReqProduct.java
@@ -1,15 +1,31 @@
 package numble.server.timedeal.domain.product.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ReqProduct {
-    private String user_id;
-    private String category_code;
-    private String product_code;
-    private String product_name;
-    private String product_desc;
-    private int product_price;
-    private int sale_price;
-    private int product_amount;
+    @JsonProperty("user_id")
+    private String userId;
+
+    @JsonProperty("category_code")
+    private String categoryCode;
+
+    @JsonProperty("product_code")
+    private String productCode;
+
+    @JsonProperty("product_name")
+    private String productName;
+
+    @JsonProperty("product_desc")
+    private String productDesc;
+
+    @JsonProperty("product_price")
+    private int productPrice;
+
+    @JsonProperty("sale_price")
+    private int salePrice;
+
+    @JsonProperty("product_amount")
+    private int productAmount;
 }
