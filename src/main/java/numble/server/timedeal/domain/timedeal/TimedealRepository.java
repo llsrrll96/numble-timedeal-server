8e9418509bf53e837411e264574649f0fbabf9d3
llsrrll96 <llsrrll96@gmail.com>
2023-04-03 00:48:50 +0900

    refactor: remove file

--- src/main/java/numble/server/timedeal/domain/timedeal/TimedealRepository.java
+++
@@ -1,9 +0,0 @@
-package numble.server.timedeal.domain.timedeal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TimedealRepository extends JpaRepository<Timedeal,Long> {
-    int deleteByTimedealId(Long timedealid);
-
-    Timedeal findByTimedealId(Long timedealid);
-}
