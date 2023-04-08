9b5ed4bbea1e7c0618ad110793d1f99da2b7259f
llsrrll96 <llsrrll96@gmail.com>
2023-04-08 21:54:11 +0900

    refactor: enum 클래스로 응답 메시지 관리
    message properties를 활용

--- src/main/java/numble/server/timedeal/domain/product/controller/ProductController.java
+++ src/main/java/numble/server/timedeal/domain/product/controller/ProductController.java
@@ -33,35 +33,35 @@ public class ProductController {
     @PostMapping("/v1")
     private ResponseEntity<APIMessage<RespProduct>> createProduct(@RequestBody ReqProduct reqProduct){
         if(isAdminUser(reqProduct.getUserId())){
-            return new ResponseEntity<>(new APIMessage<>(HttpStatus.CREATED.toString(),"상품등록성공",productService.createProduct(reqProduct)),HttpStatus.CREATED);
+            return new ResponseEntity<>(new APIMessage<>(HttpStatus.CREATED.toString(),ProductResponseMessage.SAVING_PRODUCT_SUCCESS.getRespDesc(),productService.createProduct(reqProduct)),HttpStatus.CREATED);
         }
-        return new ResponseEntity<>(new APIMessage<>(HttpStatus.BAD_REQUEST.toString(), "상품등록실패",new RespProduct()),HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new APIMessage<>(HttpStatus.BAD_REQUEST.toString(), ProductResponseMessage.SAVING_PRODUCT_FAILED.getRespDesc(),new RespProduct()),HttpStatus.BAD_REQUEST);
     }
 
     @PutMapping("/v1/{productid}")
     private ResponseEntity<APIMessage<RespProduct>> updateProduct(@PathVariable Long productid, @RequestBody ReqProduct reqProduct){
         if(isAdminUser(reqProduct.getUserId())) {
-            return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),"상품수정성공",productService.updateProduct(productid, reqProduct)),HttpStatus.OK);
+            return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),ProductResponseMessage.UPDATING_PRODUCT_SUCCESS.getRespDesc(), productService.updateProduct(productid, reqProduct)),HttpStatus.OK);
         }
-        return new ResponseEntity<>(new APIMessage<>(HttpStatus.BAD_REQUEST.toString(), "상품수정실패",new RespProduct()),HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new APIMessage<>(HttpStatus.BAD_REQUEST.toString(), ProductResponseMessage.UPDATING_PRODUCT_FAILED.getRespDesc(), new RespProduct()),HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping("/v1/{productid}")
     private ResponseEntity<APIMessage<Boolean>> deleteProduct(@PathVariable Long productid){
         if(productService.deleteProduct(productid) == 0){
-            return new ResponseEntity<>(new APIMessage<>(HttpStatus.BAD_REQUEST.toString(),"상품삭제 실패",false),HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new APIMessage<>(HttpStatus.BAD_REQUEST.toString(),ProductResponseMessage.DELETING_PRODUCT_FAILED.getRespDesc(), false),HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),"상품삭제 성공",true),HttpStatus.OK);
+        return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),ProductResponseMessage.DELETING_PRODUCT_SUCCESS.getRespDesc(), true),HttpStatus.OK);
     }
 
     @GetMapping("/v1")
     private ResponseEntity<APIMessage<List<RespProduct>>> findProductList(){
-        return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),"상품리스트",productService.findProductList()),HttpStatus.OK);
+        return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),ProductResponseMessage.FINDING_PRODUCT_SUCCESS.getRespDesc(), productService.findProductList()),HttpStatus.OK);
     }
 
     @GetMapping("/v1/{productid}")
     private ResponseEntity<APIMessage<RespProduct>> findProductInfo(@PathVariable Long productid){
-        return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),"상품조회",productService.findProductInfo(productid)),HttpStatus.OK);
+        return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),ProductResponseMessage.FINDING_PRODUCT_SUCCESS.getRespDesc(),productService.findProductInfo(productid)),HttpStatus.OK);
     }
 
     /**
@@ -69,14 +69,14 @@ public class ProductController {
     * */
     @PostMapping("/v1/category")
     private ResponseEntity<APIMessage<CategoryDto>> createCategory(@RequestBody CategoryDto categoryDto){
-        return new ResponseEntity<>(new APIMessage<>(HttpStatus.CREATED.toString(),"카테고리 등록",categoryService.createCategory(categoryDto)),HttpStatus.CREATED);
+        return new ResponseEntity<>(new APIMessage<>(HttpStatus.CREATED.toString(),ProductResponseMessage.SAVING_CATEGORY_SUCCESS.getRespDesc(), categoryService.createCategory(categoryDto)),HttpStatus.CREATED);
     }
 
     @DeleteMapping("/v1/category/{code}")
     private ResponseEntity<APIMessage<Boolean>> deleteCategory(@PathVariable String code){
         if(categoryService.deleteCategory(code) == 0){
-            return new ResponseEntity<>(new APIMessage<>(HttpStatus.BAD_REQUEST.toString(),"카테고리 삭제 실패",false),HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new APIMessage<>(HttpStatus.BAD_REQUEST.toString(),ProductResponseMessage.DELETING_CATEGORY_FAILED.getRespDesc(), false),HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),"카테고리 삭제 성공",true),HttpStatus.OK);
+        return new ResponseEntity<>(new APIMessage<>(HttpStatus.OK.toString(),ProductResponseMessage.DELETING_CATEGORY_FAILED.getRespDesc(), true),HttpStatus.OK);
     }
 }
